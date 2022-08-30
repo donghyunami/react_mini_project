6d6954cb884b83c34ebc7797ec86006af38be9a9
Donghyun Lee <skyblue5030@naver.com>
2022-08-31 01:08:54 +0900

    회원가입의 index.js 파일 수정

--- 4. 인증시스템(+유효성검사)/1. 회원가입/src/index.js
+++ 4. 인증시스템(+유효성검사)/1. 회원가입/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@fortawesome/fontawesome-free/js/all.js';
 import App from './App';
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
