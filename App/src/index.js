c0142f37f8258cdf4cf6dcb6dab9add1104b9e73
Dhyunlee <skyblue5030@naver.com>
2022-10-20 16:13:10 +0900

    project들 한곳 모으기 및 App 세팅

---
+++ App/src/index.js
@@ -0,0 +1,11 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import App from './layouts/App';
+
+//App 실행
+ReactDOM.render(
+<StrictMode>
+    <App/>
+</StrictMode>,
+document.getElementById('root')
+);
---
+++ App/src/layouts/App/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App(props) {
+  return (
+    <div>
+      <header>
+        <h1>My mini projects</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App;
