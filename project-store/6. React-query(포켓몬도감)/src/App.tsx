8429d96328af50ec973b6686dd15701508b6f81b
Dhyunlee <skyblue5030@naver.com>
2022-11-10 19:50:04 +0900

    react-query 실습, 포켓몬도감 api 활용

---
+++ project-store/6. React-query(포켓몬도감)/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import IndexPage from './pages/IndexPage';
+import DetailPage from './pages/DetailPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path=":id" element={<DetailPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
